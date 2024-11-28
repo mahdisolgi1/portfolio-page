67bc73cbdec1b866829770f5ddd0020b57fa53aa
mahdisolgi1 <vass1383@gmail.com>
2024-11-28 17:03:43 +0330

    refactor: extract testimonials from App page into a new Testimonials component

--- src/page/App.jsx
+++ src/page/App.jsx
@@ -7,34 +7,7 @@ function App() {
     <>
       <Header />
 
-      <main>
-        <div className="testimonial-Nav-text">
-          <h2 id="testimonoals" className="secondary-heading text-center">
-            Testiomonials
-          </h2>
-          <p className=" subheading add-margin text-center">
-            See the testiomonials for me
-          </p>
-        </div>
-        <div className="tesimonial-box">
-          {projectRecommendations.map((index) => (
-            <div className="tesimonial" key={index.id}>
-              <div className="img-box-empty-profile">
-                <img
-                  className="img-empty-profile"
-                  src={index.image}
-                  alt="empty-profile"
-                />
-              </div>
-              <p className="recommandedBy">
-                {index.name}
-                <br /> ({index.role})
-              </p>
-              <blockquote className="blockquote">{index.text}</blockquote>
-            </div>
-          ))}
-        </div>
-      </main>
+      <main></main>
 
       <Footer />
     </>
