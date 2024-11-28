34972c5852c70250492ff3e21b893e23beda4a48
mahdisolgi1 <vass1383@gmail.com>
2024-11-28 17:08:53 +0330

    feat(App): use extracted components inside the App page

--- src/page/App.jsx
+++ src/page/App.jsx
@@ -1,13 +1,19 @@
-import { projectRecommendations, skills, services } from "../data/data";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Portfolio from "../components/Portfolio";
+import Skills from "../components/Skills";
+import Testimonials from "../components/Testimonials";
 
 function App() {
   return (
     <>
       <Header />
 
-      <main></main>
+      <main>
+        <Skills />
+        <Portfolio />
+        <Testimonials />
+      </main>
 
       <Footer />
     </>
