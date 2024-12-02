06599c257829f2b0986f704ca07286533a72aa7f
mahdisolgi1 <vass1383@gmail.com>
2024-12-02 20:46:21 +0330

    fix(Footer): remove unnecessary div with its styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,30 +10,28 @@ function Footer() {
         M.S.Dev
       </div>
 
-      <div className={styles.linksContainer}>
-        <ul className={`${styles.footer} ${styles.links}`}>
-          <li>
-            <a href="https://instagram.com/yourusername" target="_blank">
-              <i className="fab fa-instagram"></i> Instagram
-            </a>
-          </li>
-          <li>
-            <a href="https://linkedin.com/in/mahdi-solgi" target="_blank">
-              <i className="fab fa-linkedin"></i> LinkedIn
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/mahdisolgi1" target="_blank">
-              <i className="fab fa-github"></i> GitHub
-            </a>
-          </li>
-          <li>
-            <a href="mailto:mahdi.solgi.dev@gmail.com">
-              <i className="fas fa-envelope"></i> Gmail
-            </a>
-          </li>
-        </ul>
-      </div>
+      <ul className={`${styles.footer} ${styles.links}`}>
+        <li>
+          <a href="https://instagram.com/yourusername" target="_blank">
+            <i className="fab fa-instagram"></i> Instagram
+          </a>
+        </li>
+        <li>
+          <a href="https://linkedin.com/in/mahdi-solgi" target="_blank">
+            <i className="fab fa-linkedin"></i> LinkedIn
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/mahdisolgi1" target="_blank">
+            <i className="fab fa-github"></i> GitHub
+          </a>
+        </li>
+        <li>
+          <a href="mailto:mahdi.solgi.dev@gmail.com">
+            <i className="fas fa-envelope"></i> Gmail
+          </a>
+        </li>
+      </ul>
     </footer>
   );
 }
